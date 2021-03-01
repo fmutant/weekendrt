0c196d4c8faaddfc29a8928e39afb9d3e4c189ab
fmutant <fmutant@users.noreply.github.com>
2021-03-01 21:17:19 +0200

    Update main.rs
    chapter 7

--- main.rs
+++ main.rs
@@ -1,7 +1,35 @@
+use rand::Rng;
+
 const F32_INFINITY: f32 = f32::INFINITY;
 const PI: f32 = 3.1415926535897932385f32;
 
-fn degrees_to_radians(degrees: f32) -> f32 { return degrees * PI / 180.0f32; }
+fn degrees_to_radians(degrees: f32) -> f32 {
+	return degrees * PI / 180.0f32;
+}
+
+fn random_float() -> f32 {
+	let mut rng = rand::thread_rng();
+	rng.gen::<f32>()
+}
+
+fn random_float_min_max(min: f32, max: f32) -> f32 {
+	min + (max - min) * random_float()
+}
+
+fn clamp(x: f32, min: f32, max: f32) -> f32 {
+	if x < min { return min; }
+	if max < x { return max; }
+	return x;
+}
+fn clampv(v: &Vec3, min: f32, max: f32) -> Vec3 {
+	Vec3::new(clamp(v.x(), min, max), clamp(v.y(), min, max), clamp(v.z(), min, max))
+}
+
+fn write_color(clr: &Vec3, samples_count: i32) {
+	let scale: f32 = 1.0f32 / (samples_count as f32);
+	let rgb: Vec3 = *clr * scale;
+	println!("{}", clampv(&rgb, 0.0f32, 0.9999f32));
+}
 
 #[derive(Copy, Clone)]
 struct Vec3 {
@@ -35,6 +63,13 @@ type HittableVec = Vec<IHittable>;
 
 struct HittableList {
 	m_objects: HittableVec,
+}
+
+struct Camera {
+	m_origin: Vec3,
+	m_lower_left_corner: Vec3,
+	m_horizontal: Vec3,
+	m_vertical: Vec3,
 }
 
 impl Vec3 {
@@ -283,6 +318,28 @@ impl Hittable for HittableList {
 	}
 }
 
+impl Camera {
+	fn new() -> Camera {
+		let aspect_ratio: f32 = 16.0f32 / 9.0f32;
+		let viewport_height: f32 = 2.0f32;
+		let viewport_width: f32 = viewport_height * aspect_ratio;
+		let focal_length: f32 = 1.0f32;
+		let origin: Vec3 = Vec3::new(0.0f32, 0.0f32, 0.0f32);
+		let horizontal: Vec3 = Vec3::new(viewport_width, 0.0f32, 0.0f32);
+		let vertical: Vec3 = Vec3::new(0.0f32, viewport_height, 0.0f32);
+		
+		Camera {
+			m_origin: origin,
+			m_horizontal: horizontal,
+			m_vertical: vertical,
+			m_lower_left_corner: origin - horizontal * 0.5f32 - vertical * 0.5f32 - Vec3::new(0.0f32, 0.0f32, focal_length),
+		}
+	}
+	fn get_ray(&self, u: f32, v: f32) -> Ray {
+		Ray::new(&self.m_origin, &(self.m_lower_left_corner + self.m_horizontal * u + self.m_vertical * v - self.m_origin))
+	}
+}
+
 fn ray_color(r: &Ray, world: &dyn Hittable) -> Vec3 {
 	let mut rec: HitRecord = HitRecord::new();
 	if world.hit(r, 0.0f32, F32_INFINITY, &mut rec) {
@@ -300,6 +357,7 @@ fn main() {
 	let image_height: u32 = ((image_width as f32) / aspect_ratio) as u32;
 	let image_width_inv : f32 = 1.0f32 / (image_width - 1u32) as f32;
 	let image_height_inv : f32 = 1.0f32 / (image_height - 1u32) as f32;
+	let samples_count: i32 = 100;
 	
 	//world
 	let sphere0 = Box::new( Sphere::new(&Vec3::new(0.0f32, 0.0f32, -1.0f32), 0.5f32) );
@@ -310,15 +368,7 @@ fn main() {
 	world.m_objects.push(sphere0);
 	
 	//camera
-	let viewport_height: f32 = 2.0f32;
-	let viewport_width: f32 = viewport_height * aspect_ratio;
-	let focal_length: f32 = 1.0f32;
-	
-	let origin: Vec3 = Vec3::new(0.0f32, 0.0f32, 0.0f32);
-	let horizontal: Vec3 = Vec3::new(viewport_width, 0.0f32, 0.0f32);
-	let vertical: Vec3 = Vec3::new(0.0f32, viewport_height, 0.0f32);
-	let lower_left_corner: Vec3 = origin - horizontal * 0.5f32 - vertical * 0.5f32 - Vec3::new(0.0f32, 0.0f32, focal_length);
-	let direction_origin: Vec3 = lower_left_corner - origin;
+	let cam: Camera = Camera::new();
 	
 	//render
 	println!("P3");
@@ -328,12 +378,18 @@ fn main() {
 	for j in (0u32..image_height).rev() {
 		eprint!("\rScanlines remaining: {0}", j);
 		for i in 0..image_width {
-			let u = (i as f32) * image_width_inv;
-			let v = (j as f32) * image_height_inv;
-			let direction: Vec3 = direction_origin + horizontal * u + vertical * v;
-			let r: Ray = Ray::new(&origin, &direction);
-			let clr: Vec3 = ray_color(&r, &world);
-			println!("{}", clr);
+			let mut pixel_color: Vec3 = Vec3::new(0.0f32, 0.0f32, 0.0f32);
+			for s in 0..samples_count {
+				let u = (i as f32 + random_float()) * image_width_inv;
+				let v = (j as f32 + random_float()) * image_height_inv;
+				let r: Ray = cam.get_ray(u, v);
+				pixel_color += ray_color(&r, &world);
+				//let direction: Vec3 = direction_origin + horizontal * u + vertical * v;
+				//let r: Ray = Ray::new(&origin, &direction);
+				//let clr: Vec3 = ray_color(&r, &world);
+				//println!("{}", clr);
+			}
+			write_color(&pixel_color, samples_count);
 		}
 	}
 	eprintln!("Done!");
